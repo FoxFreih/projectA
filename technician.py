343a55436b01f283963a4eaa1861876f602abdf8
AmalSaleh <amalfreih1996@gmail.com>
2019-11-21 21:48:06 +0200

    adding column of comboboxs in the technician screen

--- technician.py
+++ technician.py
@@ -49,31 +49,47 @@ from tkinter import *
 from tksheet import Sheet
 import pandas as pd
 import design
+import tkinter as tk
+from tkinter import ttk
 
 
 class TechnicianScreen(design.Page):
     def __init__(self, app, root, *args, **kwargs):
         design.Page.__init__(self, app, root, * args, **kwargs)
         self.height = 600
-        self.width = 1000
+        self.width = 1050
         self.title = "Technician schedule"
         self.grid_columnconfigure(0, weight =1)
         self.grid_rowconfigure(0, weight =1)
-        cols = pd.read_csv("customerissue.csv")
-        Cols=cols[['product','issue','place','time']]
-        row_count = sum(1 for row in cols)
+        cols = pd.read_csv("shibuts.csv")
+        Cols=cols[["product","issue","location","time","critical/notCritical"]]
+        row_count = len(cols)
+        print(row_count)
         self.data = [Cols.iloc[i] for i in range(row_count)]
-        #return cols.iloc[num]
-        #self.data = [[f"Row {r} Column {c}" for c in range(5)] for r in range(5)]
+        labelTop = tk.Label(self,
+                            text="Issue status",font=('Times New Roman', 14))
+        labelTop.grid(column=0, row=0)
+        labelTop.place(relx=0.830, rely=0.075)
+        for i in range(row_count):
+            self.comboExample = ttk.Combobox(self,
+                                    values=[
+                                        "treated",
+                                        "not treated",
+                                        "becomes normal"])
+            self.comboExample.grid(column=0, row=1)
+            self.comboExample.place(relx=0.830, rely=0.120+(i*0.04), relheight=0.04
+                              , relwidth=0.140)
+            self.comboExample.current(0)
         self.sdem = Sheet(self,
                           align = "w",
                           header_align = "center",
                           row_index_align = "center",
                           show = True,
-                          column_width = 150,
+                          column_width = 140,
                           row_index_width = 50,
                           data_reference = self.data,
-                          headers=["Product","IssueDescription","Location","Time"])
+                          headers=["Product","IssueDescription","Location","Time","**Critical/*Normal"],
+                          )
         self.sdem.enable_bindings(("single",
                                    "drag_select",
                                    "column_drag_and_drop",
@@ -88,11 +104,14 @@ class TechnicianScreen(design.Page):
                                    "row_height_resize",
                                    "double_click_row_resize"))
         self.sdem.edit_bindings(True)
-        self.sdem.grid(row = 0, column = 0, sticky = "nswe")
+        self.sdem.grid(row = 0, column = 0,sticky="nswe")
+        self.sdem.place(relx=0.05, rely=0.067, relheight=0.751
+                            , relwidth=0.780)
         self.sdem.highlight_cells(row = 0, column = 0, bg = "orange", fg = "blue")
         self.sdem.highlight_cells(row = 0, bg = "orange", fg = "blue", canvas = "row_index")
         self.sdem.highlight_cells(column = 0, bg = "orange", fg = "blue", canvas = "header")
         self.send= Button(self, text="submit", font=('Times New Roman', 14), bg='green', fg='white', padx=5,
                        command=self.save).place(relx=0.5, rely=0.5, anchor=CENTER)
     def save(self):
-        pass
+        print(self.sdem.add_row_selection(1))
+        self.sdem.anything_selected()
