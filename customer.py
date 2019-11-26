cd1c680e8579ec5308ffedc085738e7e5fd45f00
jscy <abohdaedyehya5@gmail.com>
2019-11-26 11:42:59 +0200

    fixing

--- customer.py
+++ customer.py
@@ -3,7 +3,11 @@ import tkinter as tk
 from tkinter import ttk
 import pandas as pd
 import design
-
+def genID():
+    cols = pd.read_csv("customerissues.csv")
+    id= cols.iloc[-1]
+  #  print(id)
+    return str(int(16)+1)
 # reading single line from csv file
 def readcsv(num):
     # openning file out of the function
@@ -68,12 +72,15 @@ class IssueRequest(design.Page):
         compo2.grid(column=1, row=2, padx=x, pady=y)
 
     def click(self):
+        from Funcfile import  addresTocoordinates
         customerIssue = {"name": self.name.get(),
                          "product": self.product.get(),
                          "issue": self.issue.get(),
                          "place": self.my_place.get(),
                          "time": time.asctime(),
-                         "taken": 0}
+                         "taken": 0,
+                         "coordinate": addresTocoordinates(self.my_place.get()),
+                         "ciID":genID()}
         df = pd.DataFrame([customerIssue])
         df.to_csv('customerissue.csv', mode='a', index=False, header=0)
 
